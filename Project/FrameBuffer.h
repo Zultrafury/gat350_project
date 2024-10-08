62a0a16a38e70b2c26c91be3fb761177a68dd8c9
Zultrafury <zultrafury@gmail.com>
2024-10-08 04:28:54 -0600

    draw primitives

--- Project/FrameBuffer.h
+++ Project/FrameBuffer.h
@@ -34,7 +34,151 @@ public:
 
     void DrawPoint(int x, int y, const clr& color)
     {
-        m_buffer.at(x + y * m_width) = color;
+        if (x > 0 && x < m_width && y > 0 && y < m_height)
+        {
+            m_buffer.at(x + y * m_width) = color;
+        }
+    }
+    
+    void DrawLine(int x1, int y1, int x2, int y2, const clr& color)
+    {
+        if (x2 < x1) {
+            swap(x1,x2);
+            swap(y1,y2);
+        }
+        
+        int m_new = 2 * (y2 - y1); 
+        int slope_error_new = m_new - (x2 - x1);
+        float slope = static_cast<float>(y2 - y1) / static_cast<float>(x2 - x1);
+    
+        if (slope > 1)
+        {
+            if (y2 < y1) {
+                swap(x1,x2);
+                swap(y1,y2);
+            }
+            m_new = 2 * (x2 - x1); 
+            slope_error_new = m_new - (y2 - y1);
+            
+            for (int x = x1, y = y1; y <= y2; y++) { 
+                DrawPoint(x,y,color);
+            
+                slope_error_new += m_new;
+            
+                if (slope_error_new >= 0) { 
+                    x++; 
+                    slope_error_new -= 2 * (y2 - y1); 
+                } 
+            }
+        }
+        else if (slope < -1)
+        {
+            if (y2 < y1) {
+                swap(x1,x2);
+                swap(y1,y2);
+            }
+            m_new = 2 * (x1 - x2); 
+            slope_error_new = m_new - (y2 - y1);
+            
+            for (int x = x1, y = y1; y <= y2; y++) { 
+                DrawPoint(x,y,color);
+            
+                slope_error_new += m_new;
+            
+                if (slope_error_new >= 0) { 
+                    x--; 
+                    slope_error_new -= 2 * (y2 - y1); 
+                } 
+            }
+        }
+        else
+        {
+            if (x1 == x2)
+            {
+                if (y1 > y2)
+                {
+                    swap(y1,y2);
+                }
+                for (int x = x1, y = y1; y <= y2; y++) { 
+                    DrawPoint(x,y,color);
+                }
+            }
+            else if (y1 > y2)
+            {
+                m_new = 2 * (y1 - y2); 
+                for (int x = x1, y = y1; x <= x2; x++) { 
+                    DrawPoint(x,y,color);
+            
+                    slope_error_new += m_new;
+            
+                    if (slope_error_new >= 0) { 
+                        y--; 
+                        slope_error_new -= 2 * (x2 - x1); 
+                    } 
+                }
+            }
+            else
+            {
+                for (int x = x1, y = y1; x <= x2; x++) { 
+                    DrawPoint(x,y,color);
+            
+                    slope_error_new += m_new;
+            
+                    if (slope_error_new >= 0) { 
+                        y++; 
+                        slope_error_new -= 2 * (x2 - x1); 
+                    } 
+                }
+            }
+        }
+    }
+
+    void DrawCircle(int xc, int yc, int radius, const clr& color)
+    {
+        int x = 0, y = radius;
+        int d = 3 - 2 * radius;
+
+        DrawPoint(xc+x, yc+y, color);
+        DrawPoint(xc-x, yc+y, color);
+        DrawPoint(xc+x, yc-y, color);
+        DrawPoint(xc-x, yc-y, color);
+        DrawPoint(xc+y, yc+x, color);
+        DrawPoint(xc-y, yc+x, color);
+        DrawPoint(xc+y, yc-x, color);
+        DrawPoint(xc-y, yc-x, color);
+        
+        while (y >= x){
+      
+            // check for decision parameter
+            // and correspondingly 
+            // update d, y
+            if (d > 0) {
+                y--; 
+                d = d + 4 * (x - y) + 10;
+            }
+            else
+                d = d + 4 * x + 6;
+
+            // Increment x after updating decision parameter
+            x++;
+        
+            // Draw the circle using the new coordinates
+            DrawPoint(xc+x, yc+y, color);
+            DrawPoint(xc-x, yc+y, color);
+            DrawPoint(xc+x, yc-y, color);
+            DrawPoint(xc-x, yc-y, color);
+            DrawPoint(xc+y, yc+x, color);
+            DrawPoint(xc-y, yc+x, color);
+            DrawPoint(xc+y, yc-x, color);
+            DrawPoint(xc-y, yc-x, color);
+        }
+    }
+
+    void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, const clr& color)
+    {
+        DrawLine(x1,y1,x2,y2,color);
+        DrawLine(x2,y2,x3,y3,color);
+        DrawLine(x1,y1,x3,y3,color);
     }
 
 public:
--- Project/Main.cpp
+++ Project/Main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <SDL.h>
+#include <thread>
 
 #include "FrameBuffer.h"
 #include "Renderer.h"
@@ -13,23 +14,50 @@ int main(int argc, char* argv[])
     renderer.Initialize();
     renderer.CreateWindow(900,600);
     FrameBuffer fbuff = FrameBuffer(renderer,renderer.width,renderer.height);
+    auto startnano = std::chrono::high_resolution_clock::now();
 
     while(true)
     {
-        renderer.Draw();
-        fbuff.Clear({ 0,0,0,255 });
-        
-        for (int i = 0; i < 1000; i++)
+        auto nextnano = std::chrono::high_resolution_clock::now();
+        auto nanointerval = std::chrono::duration_cast<std::chrono::nanoseconds>(nextnano-startnano).count();
+        if (nanointerval > (1000000000/1))
         {
-            int x = rand()%renderer.width;
-            int y = rand()%renderer.height;
-            //cout << x << ", " << y << endl;
-            fbuff.DrawPoint(x,y,{ 255,0,255,255 });
-        }
+            startnano = nextnano;
+            renderer.Draw();
+            fbuff.Clear({ 0,0,0,255 });
+            SDL_Color color = { 255,0,255,255 };
+        
+            for (int i = 0; i < 3; i++)
+            {
+                int x = rand()%renderer.width;
+                int y = rand()%renderer.height;
+                int r = rand()%25+1;
+                int x1 = rand()%renderer.width;
+                int y1 = rand()%renderer.height;
+                int x2 = rand()%renderer.width;
+                int y2 = rand()%renderer.height;
+                int _x1 = rand()%renderer.width;
+                int _y1 = rand()%renderer.height;
+                int _x2 = rand()%renderer.width;
+                int _y2 = rand()%renderer.height;
+                int _x3 = rand()%renderer.width;
+                int _y3 = rand()%renderer.height;
+                //cout << x << ", " << y << endl;
+                //fbuff.DrawLine(60,40,80,0,{ 255,0,0,255 });
+                //fbuff.DrawLine(0,0,20,40,{ 0,255,0,255 });
+                //fbuff.DrawLine(80,80,60,40,{ 0,0,255,255 });
+                //fbuff.DrawLine(0,80,20,40,{ 255,0,255,255 });
+                fbuff.DrawCircle(x,y,r,{ 255,0,0,255 });
+                //fbuff.DrawPoint(_x1,_y1,{ 255,0,0,255 });
+                //fbuff.DrawPoint(_x1,_y1,{ 0,0,255,255 });
+                fbuff.DrawLine(x1,y1,x2,y2,color);
+                fbuff.DrawTriangle(_x1,_y1,_x2,_y2,_x3,_y3,{ 0,0,255,255 });
+            }
 
-        fbuff.Update();
-        fbuff.CopyFrameBuffer(renderer);
-        SDL_RenderPresent(renderer.r);
+            fbuff.Update();
+            fbuff.CopyFrameBuffer(renderer);
+            SDL_RenderPresent(renderer.r);
+        }
     }
     return 0;
 }
