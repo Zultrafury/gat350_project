f771fdf6fdfa986aa79dbc18d7e9a9ecb4046b29
Zultrafury <zultrafury@gmail.com>
2024-10-15 03:06:42 -0600

    GLM integrated

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -15,14 +15,13 @@ int main(int argc, char* argv[])
     Renderer renderer = Renderer();
     renderer.Initialize();
     renderer.CreateWindow(900,600);
-    SetBlendMode(BlendMode::Alpha);
-    Image img = Image(); img.Load("angy.png"); PostProcess::Emboss(img);
+    Image img = Image(); img.Load("angy.png");
     Image imga = Image(); imga.Load("colors.png");
     FrameBuffer fbuff = FrameBuffer(renderer,renderer.width,renderer.height);
     int mx = 0; int my = 0;
     
     auto startnano = std::chrono::high_resolution_clock::now();
-
+    
     while(true)
     {
         auto nextnano = std::chrono::high_resolution_clock::now();
@@ -34,9 +33,11 @@ int main(int argc, char* argv[])
             startnano = nextnano;
             renderer.Draw();
             fbuff.Clear({ 0,0,0,255 });
-            
+
+            SetBlendMode(BlendMode::Normal);
             fbuff.DrawImage(200,50,img);
-            //fbuff.DrawImage(250,100,imga);
+            SetBlendMode(BlendMode::Additive);
+            fbuff.DrawImage(250,100,imga);
             //fbuff.DrawPoint(mx,my,{255,255,255,255});
 
             fbuff.Update();
